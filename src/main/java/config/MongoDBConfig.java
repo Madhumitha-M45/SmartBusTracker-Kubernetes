29353333f198faa5aa4c056aefe918d79fefe163
Madhumitha-M41 <mtmadhumitha2004@gmail.com>
2026-09-16 04:57:55 -0400

    Update MongoDBConfig.java
    Mongodb IP

--- src/main/java/config/MongoDBConfig.java
+++ src/main/java/config/MongoDBConfig.java
@@ -11,7 +11,7 @@ import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoDatabase;
 
 public class MongoDBConfig {
-	private static final String MONGO_URI = "mongodb://192.168.1.171:27017";
+	private static final String MONGO_URI = "mongodb://192.168.1.172:27017";
 	private static final String DATABASE_NAME = "BusTrackerDB";
 	public static final String COLLECTION_BUSES = "buses";
 	public static final String COLLECTION_TRIPS = "TripState";
